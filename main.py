b7ea9b6721346d15b33e8b987f151c39a66c41e8
helmutreinhardt <164517239+helmutreinhardt@users.noreply.github.com>
2026-01-15 20:15:26 +0100

    Mit Motor,Licht und klatschen

--- main.py
+++ main.py
@@ -1,35 +1,47 @@
-
-licht = False
-input.set_sound_threshold(SoundThreshold.LOUD, 70)
-mecanumRobotV2.set_led(LedCount.RIGHT, LedState.ON)
-mecanumRobotV2.set_led(LedCount.LEFT, LedState.ON)
-for index in range(1000):
-    mecanumRobotV2.motor(LR.UPPER_LEFT, MD.FORWARD, 30)
-    mecanumRobotV2.motor(LR.UPPER_RIGHT, MD.FORWARD, 30)
-for index2 in range(1000):
-    mecanumRobotV2.state()
-    mecanumRobotV2.set_servo(30)
-# ------------------------------------------------------------
 def on_sound_loud():
     global licht
     licht = not (licht)
-    basic.show_leds("""
-            # # # # #
-            . . # . .
-            . . # . .
-            . . # . .
-            . . # . .
-            """)
     if licht:
-        basic.clear_screen()
+        mecanumRobotV2.set_led(LedCount.RIGHT, LedState.OFF)
+        mecanumRobotV2.set_led(LedCount.LEFT, LedState.OFF)
+    else:
+        mecanumRobotV2.set_led(LedCount.LEFT, LedState.ON)
+        mecanumRobotV2.set_led(LedCount.RIGHT, LedState.ON)
+item = 0
+ms = 0
+licht = False
+input.on_sound(DetectedSound.LOUD, on_sound_loud)
+input.set_sound_threshold(SoundThreshold.LOUD, 70)
+irRemote.connect_infrared(DigitalPin.P0)
+mecanumRobotV2.set_led(LedCount.RIGHT, LedState.ON)
+mecanumRobotV2.set_led(LedCount.LEFT, LedState.ON)
+mecanumRobotV2.state()
+mecanumRobotV2.set_servo(30)
+
 def on_forever():
-    on_sound_loud()
-    for index3 in range(100000):
+    global ms, item
+    ms = sonar.ping(DigitalPin.P15, DigitalPin.P10, PingUnit.CENTIMETERS)
+    item = irRemote.return_ir_button()
+    # basic.show_number(ms)
+    if item == 21:
         mecanumRobotV2.set_servo(60)
-    for index4 in range(100000):
-        mecanumRobotV2.set_servo(30)
-    for index5 in range(100000):
+        mecanumRobotV2.motor(LR.UPPER_LEFT, MD.FORWARD, 30)
+        mecanumRobotV2.motor(LR.UPPER_RIGHT, MD.FORWARD, 30)
+        mecanumRobotV2.motor(LR.LOWER_LEFT, MD.FORWARD, 30)
+        mecanumRobotV2.motor(LR.LOWER_RIGHT, MD.FORWARD, 30)
+    elif item == 70:
         mecanumRobotV2.set_servo(0)
-    for index6 in range(100000):
+        mecanumRobotV2.motor(LR.LOWER_LEFT, MD.BACK, 30)
+        mecanumRobotV2.motor(LR.LOWER_RIGHT, MD.BACK, 30)
+        mecanumRobotV2.motor(LR.UPPER_LEFT, MD.BACK, 30)
+        mecanumRobotV2.motor(LR.UPPER_RIGHT, MD.BACK, 30)
+    elif item == 67:
+        mecanumRobotV2.set_servo(0)
+        mecanumRobotV2.motor(LR.UPPER_LEFT, MD.FORWARD, 30)
+        mecanumRobotV2.motor(LR.UPPER_RIGHT, MD.BACK, 30)
+        mecanumRobotV2.motor(LR.LOWER_LEFT, MD.BACK, 30)
+        mecanumRobotV2.motor(LR.LOWER_RIGHT, MD.FORWARD, 30)
+    else:
+        mecanumRobotV2.state()
         mecanumRobotV2.set_servo(30)
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,44 +1,50 @@
+let item = 0
+let ms = 0
 let licht = false
-input.setSoundThreshold(SoundThreshold.Loud, 70)
-mecanumRobotV2.setLed(LedCount.Right, LedState.ON)
-mecanumRobotV2.setLed(LedCount.Left, LedState.ON)
-for (let index = 0; index < 1000; index++) {
-    mecanumRobotV2.Motor(LR.Upper_left, MD.Forward, 30)
-    mecanumRobotV2.Motor(LR.Upper_right, MD.Forward, 30)
-}
-for (let index2 = 0; index2 < 1000; index2++) {
-    mecanumRobotV2.state()
-    mecanumRobotV2.setServo(30)
-}
-//  ------------------------------------------------------------
-function on_sound_loud() {
+input.onSound(DetectedSound.Loud, function on_sound_loud() {
     
     licht = !licht
-    basic.showLeds(`
-            # # # # #
-            . . # . .
-            . . # . .
-            . . # . .
-            . . # . .
-            `)
     if (licht) {
-        basic.clearScreen()
+        mecanumRobotV2.setLed(LedCount.Right, LedState.OFF)
+        mecanumRobotV2.setLed(LedCount.Left, LedState.OFF)
+    } else {
+        mecanumRobotV2.setLed(LedCount.Left, LedState.ON)
+        mecanumRobotV2.setLed(LedCount.Right, LedState.ON)
     }
     
-}
-
+})
+input.setSoundThreshold(SoundThreshold.Loud, 70)
+irRemote.connectInfrared(DigitalPin.P0)
+mecanumRobotV2.setLed(LedCount.Right, LedState.ON)
+mecanumRobotV2.setLed(LedCount.Left, LedState.ON)
+mecanumRobotV2.state()
+mecanumRobotV2.setServo(30)
 basic.forever(function on_forever() {
-    on_sound_loud()
-    for (let index3 = 0; index3 < 100000; index3++) {
+    
+    ms = sonar.ping(DigitalPin.P15, DigitalPin.P10, PingUnit.Centimeters)
+    item = irRemote.returnIrButton()
+    //  basic.show_number(ms)
+    if (item == 21) {
         mecanumRobotV2.setServo(60)
-    }
-    for (let index4 = 0; index4 < 100000; index4++) {
-        mecanumRobotV2.setServo(30)
-    }
-    for (let index5 = 0; index5 < 100000; index5++) {
+        mecanumRobotV2.Motor(LR.Upper_left, MD.Forward, 30)
+        mecanumRobotV2.Motor(LR.Upper_right, MD.Forward, 30)
+        mecanumRobotV2.Motor(LR.Lower_left, MD.Forward, 30)
+        mecanumRobotV2.Motor(LR.Lower_right, MD.Forward, 30)
+    } else if (item == 70) {
         mecanumRobotV2.setServo(0)
-    }
-    for (let index6 = 0; index6 < 100000; index6++) {
+        mecanumRobotV2.Motor(LR.Lower_left, MD.Back, 30)
+        mecanumRobotV2.Motor(LR.Lower_right, MD.Back, 30)
+        mecanumRobotV2.Motor(LR.Upper_left, MD.Back, 30)
+        mecanumRobotV2.Motor(LR.Upper_right, MD.Back, 30)
+    } else if (item == 67) {
+        mecanumRobotV2.setServo(0)
+        mecanumRobotV2.Motor(LR.Upper_left, MD.Forward, 30)
+        mecanumRobotV2.Motor(LR.Upper_right, MD.Back, 30)
+        mecanumRobotV2.Motor(LR.Lower_left, MD.Back, 30)
+        mecanumRobotV2.Motor(LR.Lower_right, MD.Forward, 30)
+    } else {
+        mecanumRobotV2.state()
         mecanumRobotV2.setServo(30)
     }
+    
 })
